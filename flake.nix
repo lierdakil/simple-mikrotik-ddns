14490b0ce3103a496804c6e27edceca82c168ace
Nikolay Yakimov <root@livid.pp.ru>
2024-03-04 22:35:59 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [flake-utils.lib.system.x86_64-linux] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+      in
+      with pkgs;
+      {
+        inherit pkgs;
+        devShells.default = mkShell {
+          buildInputs = [
+            rustc
+            cargo
+            clippy
+            rust-analyzer
+            rustfmt
+          ];
+          # Environment variables
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LD_LIBRARY_PATH = "${openssl.out}/lib:${zlib}/lib:${systemd}/lib";
+        };
+        packages.default = rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          version = manifest.version;
+          src = lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          # cargoSha256 = "sha256-kDOopnsFzshSXsA6XGpcMsJ92Rno4pqBQkZ5ES54JbM="; #lib.fakeSha256;
+          nativeBuildInputs = [ pkg-config ];
+          buildInputs = [ systemd openssl libssh2 ];
+        };
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+      }
+    );
+}
